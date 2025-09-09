b56b8252b5fb9c0a2f624cb5fa7e05c72f7256ed
kornelikkk2 <kornelikartvelishvili@yandex.ru>
2025-09-09 12:48:57 +0300

    Delete app/config.py

--- app/config.py
+++
@@ -1,8 +0,0 @@
-# Configuration file for AI API providers
-mistral_params = {
-    "api_key": "Hg4KFvGdU3DaLLsUK1jtVfz9L8xl6xUX",
-    "model": "mistral-medium-latest",
-    "temperature": 0.1
-}
-
-
